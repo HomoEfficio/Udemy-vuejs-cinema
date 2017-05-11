5eb5a946d058ee4a804ac41393747a1e04fea9bf
Oh Myung Woon <omwomw@sk.com>
2017-05-11 12:31:04 +0900

    Vue Component 기초 적용
    - Component는 여러 인스턴스로 생성되어 재사용될 수 있으므로
    - data 가 {} 객체가 아니라 함수여야 한다.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,31 @@ import './style.scss';
 
 new Vue({
     el: '#app',
-    data: {
-        msg: 'Hello World'
+    components: {
+        'movie-list': {
+            template: `<div id='movie-list'>
+                           <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                       </div>`,
+            data: function() {  // Component는 여러 인스턴스로 생성되어 재사용 될 수 있으므로 data가 함수여야 한다.
+                return {
+                    movies: [
+                        {
+                            title: 'Pulp Fiction',
+                        },
+                        {
+                            title: 'Home Alone',
+                        },
+                        {
+                            title: 'Austin Powers',
+                        },
+                    ]
+                }
+            }
+        },
+        'movie-filter': {
+            template: `<div id='movie-filter'>
+                           <h2>Filter Results</h2>
+                       </div>`
+        }
     }
 });
