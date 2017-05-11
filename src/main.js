e3cd2697a6132990a7f35d064d5cc907ebe212d2
Oh Myung Woon <omwomw@sk.com>
2017-05-11 12:44:22 +0900

    prop에 v-bind 적용

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ new Vue({
             template: `<div id='movie-filter'>
                            <h2>Filter Results</h2>
                            <div class="filter-group">
-                               <check-filter v-for="genre in genres" title="My Title"></check-filter>                         
+                               <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>                         
                            </div>
                        </div>`,
             components: {
